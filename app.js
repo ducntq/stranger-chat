b9f0c81bdf6129a329ec246a4adfef479f01d2ed
Đức Nguyễn <ducntq@gmail.com>
2014-07-17 13:57:47 +0700

    Hoàn thiện các chức năng cơ bản

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ var favicon = require('static-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var redis = require("redis"),
+    client = redis.createClient();
 var routes = require('./routes');
-var users = require('./routes/user');
+
+// use server 1
+client.select(1);
 
 var app = express();
+var server = http.Server(app);
+server.listen(3000);
+var io = require('socket.io')(server);
+
+var users = {};
+var sockets = {};
+var queueSetName = 'queue_users';
+client.flushdb();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +35,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(app.router);
 
 app.get('/', routes.index);
-app.get('/users', users.list);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
@@ -55,5 +65,86 @@ app.use(function(err, req, res, next) {
     });
 });
 
+io.on('connection', function(socket) {
+    var partnerSocket = null;
+    function getPartner() {
+        var partnerId = users[socket.id];
+        if (partnerId !== undefined) {
+            var partnerSocket = sockets[partnerId];
+            if (partnerSocket !== undefined) return partnerSocket;
+        }
 
-module.exports = app;
+        return null;
+    }
+
+    function noPartner() {
+        socket.emit('no_partner');
+        partnerSocket = null;
+    }
+
+    sockets[socket.id] = socket;
+    socket.on('ready_to_find', function() {
+        if (partnerSocket = getPartner()) {
+            partnerSocket.emit('partner_left');
+        }
+        console.log('Client ready: ' + socket.id);
+        client.sadd(queueSetName, socket.id);
+    });
+
+    socket.on('send_msg', function(data) {
+        if (partnerSocket = getPartner()) {
+        } else {
+            noPartner();
+        }
+    });
+
+    socket.on('typing', function(data) {
+        if (partnerSocket = getPartner()) {
+            partnerSocket.emit('partner_typing', data);
+        } else {
+            noPartner();
+        }
+    });
+
+    socket.on('not_typing', function(data) {
+        if (partnerSocket = getPartner()) {
+            partnerSocket.emit('partner_not_typing', data);
+        } else {
+            noPartner();
+        }
+    });
+
+    socket.on('disconnect', function() {
+        console.log('Client disconnected: ' + socket.id);
+        if (partnerSocket = getPartner()) {
+            partnerSocket.emit('partner_left');
+            delete users[partnerSocket.id];
+        }
+        client.srem(queueSetName, socket.id);
+        if (sockets[socket.id] !== undefined) delete sockets[socket.id];
+        delete users[socket.id];
+    });
+});
+
+// Ghép đôi mỗi 1s
+setInterval(function() {
+    client.scard(queueSetName, function(err, count) {
+        if (err) throw err;
+
+        if (count > 1) {
+            client.spop(queueSetName, function(err1, client1){
+                if (err1) throw err1;
+
+                client.spop(queueSetName, function(err2, client2){
+                    if(sockets[client1] !== undefined && sockets[client2] !== undefined) {
+                        console.log('Matched: ' + client1 + ' vs ' + client2);
+                        users[client1] = client2;
+                        users[client2] = client1;
+                        sockets[client1].emit('ready');
+                        sockets[client2].emit('ready');
+                    }
+                });
+            });
+        }
+    });
+}, 1000);
---
+++ public/js/main.js
@@ -0,0 +1,88 @@
+(function(socket, $) {
+    var $txtInput = $('#txt-input'),
+        $status = $('#status'),
+        $btnNew = $('#btn-new'),
+        $btnSubmit = $('#btn-submit'),
+        $content = $('#content'),
+        typingTimeout;
+
+    function disableInput(toggle){
+        $txtInput.attr('disabled', toggle);
+        $btnSubmit.attr('disabled', toggle);
+    }
+
+    function insertMessage(data, self) {
+        var chatClass = self ? 'bg-success' : 'bg-info';
+        var $chatItem = $('<p />').addClass('chat-item').addClass(chatClass);
+        $('<span />').addClass('sender').text(self ? "Bạn:" : "Người lạ:").appendTo($chatItem);
+        $('<span />').addClass('message').text(data.message).appendTo($chatItem);
+        $chatItem.appendTo($content);
+        $content.scrollTop(1e10);
+    }
+
+    function partnerLeft() {
+        $('<p />').addClass('chat-item').addClass('bg-danger').text('Người lạ đã rời phòng chat').appendTo($content);
+        $content.scrollTop(1e10);
+        $status.text('Người lạ đã kết thúc chat');
+        disableInput(true);
+    }
+
+    function sendMessage() {
+        var data = {message: $txtInput.val()};
+        insertMessage(data, true);
+        socket.emit('send_msg', data);
+        $txtInput.val('');
+    }
+
+    function clearChat() {
+        $content.html('');
+    }
+
+    disableInput(true);
+
+    $btnNew.on('click', function() {
+        socket.emit('ready_to_find');
+        $status.html('Đang tìm người để chat cùng. Xin vui lòng chờ. <i class="fa fa-spinner fa-spin"></i>');
+    });
+
+    $btnSubmit.on('click', function() {
+        sendMessage();
+    });
+
+    socket.on('ready', function() {
+        $status.text('Đã tìm được người lạ để chat!');
+        clearChat();
+        disableInput(false);
+        $txtInput.focus();
+    });
+
+    socket.on('got_msg', function(data) {
+        insertMessage(data, false);
+    });
+
+    socket.on('partner_left', function() {
+        partnerLeft();
+    });
+
+    socket.on('partner_typing', function() {
+        $status.text('Người lạ đang gõ...');
+    });
+
+    socket.on('partner_not_typing', function() {
+        $status.text('');
+    });
+
+    $txtInput.on('keydown', function(e) {
+        if (e.keyCode !== 13) {
+            socket.emit('typing');
+            clearTimeout(typingTimeout);
+            typingTimeout = setTimeout(function () {
+                socket.emit('not_typing');
+            }, 3000);
+            return true;
+        } else {
+            sendMessage();
+            return false;
+        }
+    });
+})(io.connect(), jQuery);
