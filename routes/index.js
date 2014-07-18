55d56d6b2c1be2dfb2f2724cb3fb8f71ad2ca097
Đức Nguyễn <ducntq@gmail.com>
2014-07-18 10:24:30 +0700

    remove debug

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 /* GET home page. */
 exports.index = function(req, res) {
-    console.log(req.host);
     var view = req.host + '/index';
     res.render(view, { title: 'Chat với người lạ' }, function(err, html) {
         if (err) {
