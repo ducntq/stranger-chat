3e6332f9701ee7db4a914e39e7c25a7bf909881a
Đức Nguyễn <ducntq@gmail.com>
2014-07-18 09:03:17 +0700

    Load view theo hostname

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,13 @@
 /* GET home page. */
-exports.index = function(req, res){
-  res.render('index', { title: 'Chat với người lạ' });
+exports.index = function(req, res) {
+    var view = req.host + '/index';
+    console.log('got to here');
+    res.render(view, { title: 'Chat với người lạ' }, function(err, html) {
+        if (err) {
+            console.log('got to he123321re');
+            res.render('index');
+        }
+
+        res.send(html);
+    });
 };
